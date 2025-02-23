3f9958a920bbcda58162270308155f14458e1e9b
Richard M <160991580+richarrd92@users.noreply.github.com>
2025-02-23 05:45:44 -0500

    Removed backend-python directory

--- backend-python/database.py
+++
@@ -1,6 +0,0 @@
-import models
-
-# Testing database connection - must call models
-def test():
-    models.test()
-    print("Connecting to database...")
--- backend-python/main.py
+++
@@ -1,8 +0,0 @@
-import database 
-
-# Testing backend structure - must connect to database 
-def test():
-    database.test()
-    print("Backend server is firing...")
-
-test()
