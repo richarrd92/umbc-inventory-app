3889856043205955a32a142f82ebc50123d43628
ValientTrail <rzwork17@gmail.com>
2025-02-28 23:56:13 -0500

    Created frontend files with Vite React and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Welcome!</h1>
+      <div className="card">
+        <table className='table-fixed'>
+          <thead>
+            
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default App
