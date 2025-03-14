d662b4336833389c9e38623954102611b420fa83
Raymond Zegles <rzwork17@gmail.com>
2025-03-13 20:34:50 -0400

    Created frontend with Vite + React and Tailwind CSS (#8)
    * Created frontend files with Vite React and Tailwind CSS
    
    * Created pages for frontend, created table component, edited CSS for App page
    
    * Brought over backend changes, implemented two simple tables for the database tables
    
    * Created toggle for user and item tables
    
    * Updated README documentation for frontend, moved database docs folder to proper place, created CSS file for Analytics page
    
    * Updated the frontend README with command to install Axios
    
    * Fixed issue with th tag, added to allow_origins

---
+++ frontend/src/components/ui/Table.jsx
@@ -0,0 +1,25 @@
+import { Component, useState } from 'react'
+import { Link, useParams, useLocation } from 'react-router-dom'
+
+const Table = () => {
+    return(
+        <div className='relative overflow-x-auto'>
+            <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+                <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Item Name
+                        </th>
+                        <th scope='col' className='px-6 py-3'>
+                            Quantity In Stock
+                        </th>
+                        <th scope='col' className='px-6 py-3'>
+                            Restock Threshold
+                        </th>
+                    </tr>
+                </thead>
+
+            </table>
+        </div>
+    )
+}
